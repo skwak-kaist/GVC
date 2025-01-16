20a356332a29b7599ab9d9ddd6929bfdfea54af4
skwak-kaist <sw.kwak@kaist.ac.kr>
2025-01-17 00:47:56 +0900

    mpeg

--- run_scripts/run_PC3.sh
+++ run_scripts/run_PC3.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 
-./run_1110_GVC5.0_hypernerf_interp.sh all T3-9 6210 0
-./run_1110_GVC5.0_hypernerf_interp.sh all T3-10 6211 1
-./run_1110_GVC5.0_hypernerf_interp.sh all T3-11 6212 0
-./run_1110_GVC5.0_hypernerf_interp.sh all T3-12 6213 1
-./run_1110_GVC5.0_hypernerf_interp.sh all T3-13 6214 0
-./run_1110_GVC5.0_hypernerf_interp.sh all T3-14 6215 1
-./run_1110_GVC5.0_hypernerf_interp.sh all T3-15 6216 0
+./run_1118_GVC5.0_dynerf.sh all TM-9 7771 0
+./run_1118_GVC5.0_dynerf.sh all TM-10 7772 1
+./run_1118_GVC5.0_dynerf.sh all TM-11 7773 0
+./run_1118_GVC5.0_dynerf.sh all TM-12 7774 1
+./run_1118_GVC5.0_dynerf.sh all TM-13 7775 0
+./run_1118_GVC5.0_dynerf.sh all TM-14 7776 1
+./run_1118_GVC5.0_dynerf.sh all TM-15 7777 0
+./run_1118_GVC5.0_dynerf.sh all TM-16 7778 1
+
 
 

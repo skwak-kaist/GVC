7b3b01330f6af0b880ff63430e0864619688948f
skwak-kaist <sw.kwak@kaist.ac.kr>
2025-01-27 17:12:18 +0900

    dnerf

--- run_scripts/run_PC2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-./run_250123_GVC5.0_dnerf.sh all T2-1 4001 0
-./run_250123_GVC5.0_dnerf.sh all T2-2 4002 1
-./run_250123_GVC5.0_dnerf.sh all T2-3 4003 0
-./run_250123_GVC5.0_dnerf.sh all T2-4 4004 1

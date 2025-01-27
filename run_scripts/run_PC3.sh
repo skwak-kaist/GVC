a431b05f9394dd27c5aefeaac256665acdad3815
skwak-kaist <sw.kwak@kaist.ac.kr>
2025-01-27 16:43:30 +0900

    dnerf

--- run_scripts/run_PC3.sh
+++ run_scripts/run_PC3.sh
@@ -1,13 +1,6 @@
 #!/bin/bash 
 
-./run_1118_GVC5.0_dynerf.sh all TM-9 7771 0
-./run_1118_GVC5.0_dynerf.sh all TM-10 7772 1
-./run_1118_GVC5.0_dynerf.sh all TM-11 7773 0
-./run_1118_GVC5.0_dynerf.sh all TM-12 7774 1
-./run_1118_GVC5.0_dynerf.sh all TM-13 7775 0
-./run_1118_GVC5.0_dynerf.sh all TM-14 7776 1
-./run_1118_GVC5.0_dynerf.sh all TM-15 7777 0
-./run_1118_GVC5.0_dynerf.sh all TM-16 7778 1
-
-
-
+./run_250123_GVC5.0_dnerf.sh all T3-1 4001 0
+./run_250123_GVC5.0_dnerf.sh all T3-2 4002 1
+./run_250123_GVC5.0_dnerf.sh all T3-3 4003 0
+./run_250123_GVC5.0_dnerf.sh all T3-4 4004 1

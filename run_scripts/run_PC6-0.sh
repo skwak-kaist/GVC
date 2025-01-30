a7968bfb9086816b950e249d6e65833179f6e28f
skwak-kaist <sw.kwak@kaist.ac.kr>
2025-01-30 17:43:50 +0900

    rebut

--- run_scripts/run_PC6-0.sh
+++ run_scripts/run_PC6-0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-./run_250123_GVC5.0_dnerf.sh all T6-13 6113 0
-./run_250123_GVC5.0_dnerf.sh all T6-14 6114 0
+./run_250123_GVC5.0_dnerf.sh all T6-17 6213 0
+./run_250123_GVC5.0_dnerf.sh all T6-19 6214 0
 
--- run_scripts/run_PC6-1.sh
+++ run_scripts/run_PC6-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-./run_250123_GVC5.0_dnerf.sh all T6-15 6115 1
-./run_250123_GVC5.0_dnerf.sh all T6-16 6116 1
+./run_250123_GVC5.0_dnerf.sh all T6-18 6215 1
+./run_250123_GVC5.0_dnerf.sh all T6-20 6216 1
 
